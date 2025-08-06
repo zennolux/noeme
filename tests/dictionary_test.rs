647efc8bb74d9512250c8b4ebc9bdc85c833c43a
zennolux <zennolux@gmail.com>
2025-08-07 04:45:58 +0800

    test: add test for the `to_json` method which implement from  `Jsonify` trait

--- tests/dictionary_test.rs
+++ tests/dictionary_test.rs
@@ -1,5 +1,4 @@
-use anyhow::{anyhow, Error};
-use dictionary::Dictionary;
+use dictionary::{Dictionary, Jsonify};
 
 #[test]
 fn should_get_instance() {
@@ -10,15 +9,16 @@ fn should_get_instance() {
 }
 
 #[test]
-fn should_get_json() -> Result<(), Error> {
+fn specific_items_and_itself_canbe_serialized_to_json() {
     let word = "extremely";
-    let result = Dictionary::new(word)?.to_json();
+    let dict = Dictionary::new(word).unwrap();
 
-    if let Ok(_) = result {
-        Ok(())
-    } else {
-        Err(anyhow!("Failed to serialize dictionary to json string"))
-    }
+    assert!(
+        dict.pronunciation.to_json().is_ok()
+            && dict.meanings.to_json().is_ok()
+            && dict.sentences.to_json().is_ok()
+            && dict.to_json().is_ok()
+    )
 }
 
 #[test]
