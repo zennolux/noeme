d7e97b34d802857bd895f5e38d2db86a8cd31d82
zennolux <zennolux@gmail.com>
2025-08-12 02:59:35 +0800

    chore: remove debug code

--- core/src/main.rs
+++ core/src/main.rs
@@ -11,8 +11,6 @@ fn main() -> Result<()> {
     let cli = Cli::parse();
     let json_str = Explainer::from(cli.word.as_str())?.to_json()?;
 
-    fs::write(format!("./examples/{}.json", cli.word), &json_str)?;
-
     println!("{:?}", json_str);
 
     Ok(())
