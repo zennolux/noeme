447dda795ff4fc6784fe6fe7133fc6a6458651a9
zennolux <zennolux@gmail.com>
2025-08-19 01:00:13 +0800

    chore: Parse json to JS Object directly in Rust side

--- explainer-wasm/src/lib.rs
+++ explainer-wasm/src/lib.rs
@@ -1,11 +1,19 @@
 use explainer::{Explainer, Jsonify};
-use wasm_bindgen::prelude::wasm_bindgen;
+use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
 
 #[wasm_bindgen]
-pub async fn explain(word: &str) -> String {
-    Explainer::from(word)
+extern "C" {
+    #[wasm_bindgen(js_namespace = JSON)]
+    fn parse(json_str: &str) -> JsValue;
+}
+
+#[wasm_bindgen]
+pub async fn explain(word: &str) -> JsValue {
+    let result = Explainer::from(word)
         .await
         .expect("Unable to load explainer")
         .to_json()
-        .expect("Unable to serialize to json")
+        .expect("Unable to serialize to json");
+
+    parse(result.as_str())
 }
