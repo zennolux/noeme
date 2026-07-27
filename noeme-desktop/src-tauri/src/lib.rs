d1d75c893fe0111cd6c6771a7f2fe6b24f7bfb08
zennolux <zennolux@gmail.com>
2026-07-28 01:44:59 +0800

    chore: bind hotkey for screenshot feature

--- noeme-desktop/src-tauri/src/lib.rs
+++ noeme-desktop/src-tauri/src/lib.rs
@@ -7,6 +7,42 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            #[cfg(desktop)]
+            {
+                use tauri::Emitter;
+                use tauri_plugin_global_shortcut::{
+                    Code, GlobalShortcutExt, Modifiers, Shortcut, ShortcutState,
+                };
+
+                let shortcut_screenshot =
+                    //Shortcut::new(Some(Modifiers::CONTROL | Modifiers::ALT), Code::F6);
+                    Shortcut::new(Some(Modifiers::CONTROL | Modifiers::SHIFT), Code::F12);
+
+                app.handle().plugin(
+                    tauri_plugin_global_shortcut::Builder::new()
+                        .with_handler(move |app_handle, shortcut, event| {
+                            println!("You pressed {}", shortcut);
+                            if shortcut != &shortcut_screenshot {
+                                return ();
+                            }
+
+                            if event.state() != ShortcutState::Pressed {
+                                return ();
+                            }
+
+                            app_handle.emit("shortcut-screenshot", ()).expect("");
+                        })
+                        .build(),
+                )?;
+
+                app.global_shortcut()
+                    .register(shortcut_screenshot)
+                    .expect("Failed to register shortcut of screenshot");
+            }
+
+            Ok(())
+        })
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_screenshots::init())
--- noeme-desktop/src/App.tsx
+++ noeme-desktop/src/App.tsx
@@ -2,6 +2,11 @@
 //import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 import "react-image-crop/dist/ReactCrop.css";
+import {
+  isRegistered,
+  register,
+  unregister,
+} from "@tauri-apps/plugin-global-shortcut";
 
 import {
   getMonitorScreenshot,
@@ -9,6 +14,8 @@ import {
 } from "tauri-plugin-screenshots-api";
 import { WebviewWindow } from "@tauri-apps/api/webviewWindow";
 import { load } from "@tauri-apps/plugin-store";
+import { useEffect } from "react";
+import { listen } from "@tauri-apps/api/event";
 
 function App() {
   async function createScreenshotableWindow() {
@@ -48,6 +55,31 @@ function App() {
     await store.save();
   }
 
+  //@ts-ignore
+  async function registerShortcut() {
+    const shortcut = "CommandOrControl+Shift+F12";
+
+    unregister(shortcut);
+
+    if (await isRegistered(shortcut)) {
+      console.info("Aready registered!");
+      return;
+    }
+
+    await register(shortcut, async (event) => {
+      if (event.state === "Pressed") {
+        console.info("triggered");
+        await createScreenshotableWindow();
+      }
+    });
+  }
+
+  useEffect(() => {
+    listen("shortcut-screenshot", () => {
+      console.info("triggered");
+    });
+  }, []);
+
   //async function greet() {
   //  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
   //  setGreetMsg(await invoke("greet", { name }));
@@ -61,7 +93,6 @@ function App() {
         style={{ height: "400px", width: "100%" }}
         alt="Vite logo"
       />
-      <button onClick={createScreenshotableWindow}>截图</button>
     </>
   );
 }
