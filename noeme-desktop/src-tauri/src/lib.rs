cb082713d8ba1d74b46999244bfd8084319a3ade
zennolux <zennolux@gmail.com>
2026-08-08 01:25:04 +0800

    chore: setup webview window

--- noeme-desktop/src-tauri/src/lib.rs
+++ noeme-desktop/src-tauri/src/lib.rs
@@ -1,3 +1,6 @@
+use tauri::Manager;
+use tauri_plugin_positioner::{Position, WindowExt};
+
 mod commands;
 mod hotkey;
 
@@ -7,8 +10,26 @@ pub fn run() {
         .setup(|app| {
             hotkey::register(app)?;
 
+            if let Some(win) = app.get_webview_window("main") {
+                if let Ok(Some(monitor)) = win.current_monitor() {
+                    let scale_factor = monitor.scale_factor();
+                    let work_area = monitor.work_area();
+                    let logical_height = work_area.size.height as f64 / scale_factor;
+
+                    win.set_size(tauri::Size::Logical(tauri::LogicalSize {
+                        width: 400 as f64,
+                        height: logical_height * 0.9,
+                    }))?;
+
+                    win.as_ref().window().move_window(Position::RightCenter)?;
+
+                    win.show()?;
+                }
+            }
+
             Ok(())
         })
+        .plugin(tauri_plugin_positioner::init())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_screenshots::init())
