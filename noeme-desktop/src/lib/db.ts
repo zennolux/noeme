604aec1cac5f8ea361542020c036d8893e8f7d8e
zennolux <zennolux@gmail.com>
2026-08-26 01:57:59 +0800

    chore: get local words

--- noeme-desktop/src/lib/db.ts
+++ noeme-desktop/src/lib/db.ts
@@ -6,8 +6,38 @@ export enum MarkKind {
   Mastered,
 }
 
+export interface LocalWord {
+  name: string;
+  meaning: string;
+  mark: MarkKind;
+  details: Noeme | string;
+  created_at: string;
+}
+
 async function loadDatabse() {
   return await Database.load("sqlite:noeme.db");
+}
+
+export async function getLocalWords(
+  mark: MarkKind
+): Promise<{ total: number; data: Array<LocalWord> }> {
+  const db = await loadDatabse();
+
+  const sql = "SELECT {} FROM vocabularies WHERE mark = $1";
+
+  const total = (
+    await db.select<[{ total: number }]>(
+      sql.replace("{}", "count(id) as total"),
+      [mark]
+    )
+  )[0].total;
+
+  const data = await db.select<Array<LocalWord>>(
+    sql.replace("{}", "id, name, meaning, mark, created_at"),
+    [mark]
+  );
+
+  return { total, data };
 }
 
 export async function getWordDetailsFromLocal(
@@ -31,9 +61,10 @@ export async function saveNewWord(noeme: Noeme) {
   const db = await loadDatabse();
 
   return db.execute(
-    "REPLACE INTO vocabularies (name, mark, details, created_at) VALUES ($1, $2, $3, $4)",
+    "REPLACE INTO vocabularies (name, meaning, mark, details, created_at) VALUES ($1, $2, $3, $4, $5)",
     [
-      noeme.word,
+      noeme.word.toLowerCase(),
+      noeme.basic_meanings[0]?.value,
       MarkKind.Fresh,
       JSON.stringify(noeme),
       new Date().toLocaleString(),
