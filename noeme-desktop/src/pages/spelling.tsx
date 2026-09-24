d5c957f5b12a951c0faff7b78d17ae29fb65f820
zennolux <zennolux@gmail.com>
2026-09-25 01:45:26 +0800

    chore: support spelling new words

--- noeme-desktop/src/pages/spelling.tsx
+++ noeme-desktop/src/pages/spelling.tsx
@@ -3,27 +3,57 @@ import Typed from "typed.js";
 import { CgDetailsMore as IconDetails } from "react-icons/cg";
 import { MdClose as IconClose } from "react-icons/md";
 import { getCurrentWebviewWindow } from "@tauri-apps/api/webviewWindow";
+import { emit } from "@tauri-apps/api/event";
+import { getLocalWords, MarkKind, type LocalWord } from "@/lib/db";
 
 export default function Spelling() {
   const win = getCurrentWebviewWindow();
   const el = useRef(null);
-  const [word, setWord] = useState<Noeme["word"]>();
+  const [words, setWords] = useState<Array<LocalWord>>();
+  const [currentWord, setCurrentWord] = useState<Noeme["word"]>();
+
+  async function getNewWords() {
+    const { data } = await getLocalWords(MarkKind.New);
+
+    setWords(data);
+  }
 
   useEffect(() => {
-    if (!el.current || !word) {
+    if (!el.current || !currentWord) {
       return;
     }
     const typed = new Typed(el.current, {
-      strings: [word!],
-      typeSpeed: 700,
+      strings: [currentWord!],
+      typeSpeed: 600,
       loop: true,
     });
 
     return () => typed.destroy();
-  }, [el.current, word]);
+  }, [el.current, currentWord]);
 
   useEffect(() => {
-    setWord("incredible");
+    if (!words || words.length < 1) {
+      return;
+    }
+
+    let idx = 0;
+    setCurrentWord(words[idx].name);
+
+    const timer = setInterval(() => {
+      if (idx >= words.length) {
+        idx = 0;
+      } else {
+        idx++;
+      }
+
+      setCurrentWord(words[idx].name);
+
+      return () => clearInterval(timer);
+    }, 1000 * 60 * 5);
+  }, [words]);
+
+  useEffect(() => {
+    getNewWords();
   }, []);
 
   return (
@@ -35,7 +65,10 @@ export default function Spelling() {
       ></div>
       <div className="text-2xl absolute top-1/2 -translate-y-1/2 right-1 ">
         <div className="flex gap-2">
-          <IconDetails className="cursor-pointer text-gray-400 hover:text-gray-200" />
+          <IconDetails
+            className="cursor-pointer text-gray-400 hover:text-gray-200"
+            onClick={() => emit("word-recognized", currentWord)}
+          />
           <IconClose
             className="cursor-pointer text-gray-400 hover:text-gray-200"
             onClick={() => win.close()}
